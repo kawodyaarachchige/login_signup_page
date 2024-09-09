f07b22a45d3f9395d62a83c3cf6774792effeaa8
Tharushi Kawodya Arachchige <139765472+kawodyaarachchige@users.noreply.github.com>
2024-09-09 21:17:16 +0530

    t

---
+++ authDetails.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDDlzwS4_pNHOoL4tQdeChtqb72WgHFgs0",
+    authDomain: "simpleauthentication-9e62b.firebaseapp.com",
+    projectId: "simpleauthentication-9e62b",
+    storageBucket: "simpleauthentication-9e62b.appspot.com",
+    messagingSenderId: "844106200976",
+    appId: "1:844106200976:web:de5eabcf31164e5270e515"
+};
+
